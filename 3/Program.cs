5a9be7ebb7c755fde85bfccd2e9495c77df78721
Olga Volosenko <olgavolosenko@gmail.com>
2023-07-18 15:33:24 +0300

    ДЗ 3 решено

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая задаёт массив из 8 элементов 
+//  и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100);
+}
+Console.WriteLine($"[{String.Join(", ", array)}]");
