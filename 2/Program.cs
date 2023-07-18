e117cafc3a274f06bcd563ad478fce9649754ba5
Olga Volosenko <olgavolosenko@gmail.com>
2023-07-18 15:13:04 +0300

    Дз 2 решено

---
+++ 2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму 
+// цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int GetSum(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10;
+        number /= 10;
+    }
+    return result;
+}
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    string readInput = Console.ReadLine();
+    int result = Convert.ToInt32(readInput);
+    return result;
+}
+
+int number = GetInput("Введите число: ");
+Console.WriteLine($"Сумма всех чисел в {number} равна {GetSum(number)}");
